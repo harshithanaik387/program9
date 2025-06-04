8d3625c3c7c39dd60eccb2c24cf84a671ae5a3f4
harshithanaik387 <naikharshitha387@gmail.com>
2025-06-04 07:01:28 +0530

    first commit

---
+++ src/main/java/maventogradle/program9/App.java
@@ -0,0 +1,13 @@
+package maventogradle.program9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
